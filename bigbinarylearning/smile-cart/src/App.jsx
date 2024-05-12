90530d143fe4ddd25f41adf9ed2d28235ebc5135
Mohammad Ravoof <140537466+mohammadravoof@users.noreply.github.com>
2024-05-12 17:53:18 +0530

    smile cart initialized

---
+++ bigbinarylearning/smile-cart/src/App.jsx
@@ -0,0 +1,24 @@
+import logo from './assets/react.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img alt="logo" className="App-logo" src={logo} />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Learn React
+      </a>
+      <p>My first React App.</p>
+    </header>
+  </div>
+);
+
+export default App;
