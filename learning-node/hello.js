99c47820fa47f493e0e44032d6cc52c891250e75
Mohammad Ravoof <140537466+mohammadravoof@users.noreply.github.com>
2024-05-16 17:17:32 +0530

    practiced fileSystem

--- learning-node/hello.js
+++ learning-node/hello.js
@@ -1,3 +1,9 @@
 const {add, sub} = require('./math')
+const fs = require('fs')
 
 console.log("Addition ",add(1,2)," Subtraction ",sub(2,1) )
+
+// //creating a new file and adding content to it.
+// fs.writeFileSync('./fs-writen-file.txt','Hey This is done using fs.writeFilesync');
+// //deleting any file
+// fs.unlinkSync('./fs-writen-file')
