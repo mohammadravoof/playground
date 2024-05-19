d87c2912b201ec29c94530f512666a6f21784891
Mohammad Ravoof <140537466+mohammadravoof@users.noreply.github.com>
2024-05-19 16:08:47 +0530

    learnt about Url

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 const myServer = http.createServer((req, res) => {
+    if(req.url === '/favicon.ico') return res.end();
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);``
     const log = `${Date.now()}: ${req.url} New Req Received\n`;
     fs.appendFile('log.txt',log, (err, data) => {
-        switch(req.url) {
+        switch(myUrl.pathname) {
             case '/': res.end("HomePage");
             break;
-            case '/about': res.end("I am Mohammad Ravoof");
+            case '/about': 
+            const username = myUrl.query.myname
+            res.end(`Hi ${username}`);
             break;
             default: res.end("404 Not Found");
         }
