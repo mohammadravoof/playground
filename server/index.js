4839b489aa30d44b47a15a2f7a5bbcbcf9a04286
Mohammad Ravoof <140537466+mohammadravoof@users.noreply.github.com>
2024-05-19 18:06:12 +0530

    learnt about http methods
    GET
    POST
    PUT
    PATCH
    DELETE

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const url = require('url');
 const myServer = http.createServer((req, res) => {
     if(req.url === '/favicon.ico') return res.end();
     const myUrl = url.parse(req.url, true);
-    console.log(myUrl);``
-    const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    // console.log(myUrl);
+    const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
     fs.appendFile('log.txt',log, (err, data) => {
         switch(myUrl.pathname) {
             case '/': res.end("HomePage");
