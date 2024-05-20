cffb94ee9dfba6719919b9da84d4fd4a81431b98
Mohammad Ravoof <140537466+mohammadravoof@users.noreply.github.com>
2024-05-21 01:02:25 +0530

    Started project-01
    Initialized the node server using : npm init
    installed express
    created index.js
    written script start: "node index.js"
    created tasks to acomplish
    downloaded mock data from mockaroo.com
    completed the planned http tasks such as get post patch delete

---
+++ project-01/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const users = require('./MOCK_DATA.json');
+
+//Routes
+app.get('/users', (req,res) => {
+    const html = `
+    <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
+});
+
+app.get('/api/users', (req,res) => {
+    return res.json(users);
+});
+
+app
+.route('/api/users/:id')
+.get((req,res) => {
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id);
+    return res.json(user);
+})
+.patch((req,res) => {
+    // TODO: Edit the user with id
+    return res.json({ status: "edit user pending"});
+})
+.delete((req,res) => {
+    // TODO: Delete the user with id
+    return res.json({ status: "delete user pending"});
+});
+
+app.post('/api/users', (req,res) => {
+    // TODO: Create new user
+    return res.json({ status: "create user pending"});
+});
+
+app.listen(PORT, ()=> console.log(`Server started at PORT: ${PORT}`));
