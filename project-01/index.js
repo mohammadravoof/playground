83b41461bcfeeeb4525355c2f693c199ca115fbd
Mohammad Ravoof <140537466+mohammadravoof@users.noreply.github.com>
2024-05-21 20:52:10 +0530

    Completed application of get post patch del operations

--- project-01/index.js
+++ project-01/index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 const users = require('./MOCK_DATA.json');
+const fs = require('fs');
+
+// Middleware - Plugin
+app.use(express.urlencoded({ extended: false}));
 
 //Routes
+app.get('/', (req, res) => {
+    const homepage = `<h1>Home Page</h1>`;
+    return res.send(homepage);
+})
+
 app.get('/users', (req,res) => {
     const html = `
     <ul>
@@ -26,16 +35,57 @@ app
 })
 .patch((req,res) => {
     // TODO: Edit the user with id
-    return res.json({ status: "edit user pending"});
-})
+    // getId stores the Id from the given Parameters in the URL.
+    const getId = Number(req.params.id);
+
+    // body stores the body in which we've to make changes.
+    const body = req.body;
+
+    // Finding the user Id from the user array.
+    const userIndex = users.findIndex((user) => user.id === getId);
+
+    // If we found a user with its Id then gotUser stores that object.
+    const gotUser = users[userIndex];
+
+    // Here gotUser has the user Object and body has the changes we have to made.
+    const updatedUser = { ...gotUser, ...body};
+
+    // After Merging them, Update the users Array.
+    users[userIndex] = updatedUser;
+
+    // Lastly, write the changes into the json file.
+    console.log(body);
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+      return res.json({ status: "Success", updatedUser});
+    })
+  })
 .delete((req,res) => {
     // TODO: Delete the user with id
-    return res.json({ status: "delete user pending"});
+    const id = Number(req.params.id)
+
+    if(id!=-1 && users.length>=id){
+      users.splice(id-1,1);
+      fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+        if(!err){
+          return res.status(200).json({success:"User delete"});
+        }else{
+          res.status(500).json({error:"Failed to delete user"});
+        }
+    });
+  }else{
+    return res.status(404).json({error:"User not found"});
+  }  
 });
 
 app.post('/api/users', (req,res) => {
-    // TODO: Create new user
-    return res.json({ status: "create user pending"});
+    // Create new user
+    const body = req.body;
+    console.log('Body ',body);
+    users.push({ id:users.length + 1 ,...body});
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({ status: "success", id: users.length });
+    });
+
 });
 
 app.listen(PORT, ()=> console.log(`Server started at PORT: ${PORT}`));
